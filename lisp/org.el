92fbd15dae9a7d60c1a194aedd2569b1903c8a06
Edgar Flores <luisedgarflores@ciencias.unam.mx>
2025-08-16 23:43:25 -0600

    org config

--- lisp/org.el
+++ lisp/org.el
@@ -1,11 +1,45 @@
-;; Make the indentation look nicer
-(add-hook 'org-mode-hook 'org-indent-mode)
+;; org.el — put this in your org.el or your init where you keep package config
+(use-package org
+  :defer t
+  :init
+  ;; base locations (customize to your preference)
+  (setq org-directory (expand-file-name "~/org/"))
+  (setq org-default-notes-file (concat org-directory "inbox.org"))
+  ;; nicer startup
+  (setq org-startup-indented t)     ;; behave like org-indent-mode on open
+  (setq org-startup-folded 'showall) ;; or 'content / 'overview
+  (setq org-ellipsis " ▼ ")         ;; nicer fold ellipsis
+  :config
+  ;; buffer presentation
+  (add-hook 'org-mode-hook #'visual-line-mode)
+  (add-hook 'org-mode-hook #'org-indent-mode)
+  (setq org-hide-emphasis-markers t)   ;; hide *bold* markers
+  (setq org-return-follows-link t)     ;; RET follows links
 
-;; Follow the links
-(setq org-return-follows-link  t)
+  ;; source block editing/formatting
+  (setq org-src-fontify-natively t)    ;; syntax highlight code blocks
+  (setq org-src-preserve-indentation t)
+  (setq org-edit-src-content-indentation 0)
+  (setq org-src-tab-acts-natively t)
 
-;; Hide the markers so you just see bold text as BOLD-TEXT and not *BOLD-TEXT*
-(setq org-hide-emphasis-markers t)
+  ;; Babel — enable languages you want to run inside org
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((emacs-lisp . t)
+     (python . t)
+     (shell . t))) ;; add more as needed
 
-;; Wrap the lines in org mode so that things are easier to read
-(add-hook 'org-mode-hook 'visual-line-mode)
+  ;; Keep Org files tidy and searchable
+  (setq org-agenda-files (list org-directory))
+  (setq org-agenda-span 'week)
+
+  ;; Capture templates (quick notes)
+  (setq org-capture-templates
+        `(("t" "Todo" entry (file+headline ,(concat org-directory "inbox.org") "Tasks")
+           "* TODO %?\n  %i\n  %a")
+          ("n" "Note" entry (file ,(concat org-directory "notes.org"))
+           "* %U %?\n\n  %i\n  %a")))
+
+  ;; nice default appearance for headings/blocks
+  (setq org-hide-leading-stars t)
+  (setq org-pretty-entities t))
